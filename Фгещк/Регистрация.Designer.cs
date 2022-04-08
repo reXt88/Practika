c353795514c0509e77a13ae62173ae7928c4395e
RusskiZadrot <max197vip@gmail.com>
2022-04-08 15:15:31 +0300

    .

--- Фгещк/Регистрация.Designer.cs
+++ Фгещк/Регистрация.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // label1
             // 
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(291, 38);
             this.label1.Name = "label1";
